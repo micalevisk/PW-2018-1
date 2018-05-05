2676ae85dc1f9ba4e13521c041ed4dafa2ef2e5f
Micael Levi <mllc@icomp.ufam.edu.br>
2018-05-05 12:17:15 -0400

    done: posicionamento; estilo inicial

---
+++ skifree/js/models/Tabuleiro.js
@@ -0,0 +1,9 @@
+export default function Tabuleiro(largura, altura, borda = '2') {
+  this.element = document.getElementById('tabuleiro');
+  this.element.style.width  = largura + 'px';
+  this.element.style.height = altura + 'px';
+  this.element.style.borderWidth = borda + 'px';
+
+
+  this.getWidth = () => largura;
+}
