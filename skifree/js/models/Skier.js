5f814cb6ff917659d8e08ac9681798b8f0b1bf98
Micael Levi <mllc@icomp.ufam.edu.br>
2018-05-10 00:46:28 -0400

    add: lógica para alternar entre os sprites do skier parado

--- skifree/js/models/Skier.js
+++ skifree/js/models/Skier.js
@@ -2,8 +2,9 @@ const PARADO   = ['parado-esquerda', 'parado-direita'];
 const DIRECOES = ['para-esquerda','para-frente','para-direita'];
 
 function Skier(tabuleiroWidth, posLeft, qtdVidas, posTop = 60) {
-  this.direcao = 1;
+  this.direcao = 0;
   this.andando = false;
+  this.iniciou = false;
   this.metrosAndados = 0;
   this.vidasRestantes = qtdVidas;
 
@@ -11,7 +12,13 @@ function Skier(tabuleiroWidth, posLeft, qtdVidas, posTop = 60) {
   this.element.style.position = 'absolute';
   this.element.style.top = posTop + 'px';
   this.element.style.left = posLeft + 'px';
+  this.element.className = PARADO[0];
 
+
+  this.iniciar = function () {
+    this.iniciou = true;
+    this.element.className = DIRECOES[ (this.direcao = 1) ];
+  };
 
   this.setAndando = function (andando) {
     return this.andando = andando;
@@ -26,10 +33,18 @@ function Skier(tabuleiroWidth, posLeft, qtdVidas, posTop = 60) {
   };
 
   this.mudarDirecao = function (sentido) {
-    if (!this.andando) return;
+    if (!this.andando) {
+      if (!this.iniciou) {
+        if ((this.direcao + sentido >= 0)
+         && (this.direcao + sentido < PARADO.length))
+          this.element.className = PARADO[ (this.direcao += sentido) ];
+      }
+
+      return;
+    }
 
     if ((this.direcao + sentido >= 0)
-     && (this.direcao + sentido <= 2))
+     && (this.direcao + sentido < DIRECOES.length))
       this.element.className = DIRECOES[ (this.direcao += sentido) ];
   };
 
