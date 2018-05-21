160702dbd4cb7075ec5c0c356e5b84cdab649d73
micalevisk <mllc@icomp.ufam.edu.br>
2018-05-21 00:42:46 -0400

    add: atribuição dinâmica do método `colidiu`

--- skifree/js/models/Obstaculo.js
+++ skifree/js/models/Obstaculo.js
@@ -15,6 +15,23 @@ Obstaculo.prototype.spawn = function (tabuleiro, tolerancia, initialTop, initial
   this.element.style.left = (initialLeft || Math.floor(
     Math.random() * (tabuleiro.getWidth() - this.element.clientWidth + 1 + tolerancia)
   ) + this.element.clientWidth) + 'px';
+
+  const minTop = parseInt( _.getCssProperty(this.element, '--diff-min-top') );
+  if ( _.isNumeric(minTop) ) {
+    const maxTop = parseInt( _.getCssProperty(this.element, '--diff-max-top') );
+    const minLeft= parseInt( _.getCssProperty(this.element, '--diff-min-left') );
+    const maxLeft= parseInt( _.getCssProperty(this.element, '--diff-max-left') );
+
+    const topEstaEntre = _.isNumberBetween(minTop, maxTop);
+    const leftEstaEntre = _.isNumberBetween(minLeft, maxLeft);
+
+    this.colidiu = function (targetTop, targetLeft) {
+      return topEstaEntre(targetTop - parseInt(this.element.style.top))
+          && leftEstaEntre(targetLeft - parseInt(this.element.style.left));
+    };
+  } else {
+    this.colidiu = function () { return false; } // nop
+  }
 }
 
 Obstaculo.prototype.subir = function () {
