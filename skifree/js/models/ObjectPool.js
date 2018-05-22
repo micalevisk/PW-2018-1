c144221884076d74967c7262e80783290bcad496
micalevisk <mllc@icomp.ufam.edu.br>
2018-05-21 20:34:12 -0400

    rm: prints (logs) removidos

--- skifree/js/models/ObjectPool.js
+++ skifree/js/models/ObjectPool.js
@@ -13,13 +13,11 @@ ObjectPool.prototype.alloc = function (params) {
   if (this._poolFree.length <= 0) {
     // nenhum objeto a ser re-utilizado, então alocar um
     this._poolInUse.push( new this.objType(params) )
-    console.log(`in use: ${this._poolInUse.length}`);
     return this._poolInUse[ this._poolInUse.length - 1 ];
   }
 
   const reusedObj = this._poolFree.pop();
   this._poolInUse.push(reusedObj);
-  console.log(`reused: ${this._poolFree.length} left`);
   reusedObj.constructor(params);
   return reusedObj;
 }
@@ -27,7 +25,6 @@ ObjectPool.prototype.alloc = function (params) {
 ObjectPool.prototype.freeAt = function (idx) {
   const removedObj = this._poolInUse.splice(idx, 1)[0];
   this._poolFree.push(removedObj);
-  console.log(`free: ${this._poolFree.length}`);
 }
 
 Object.prototype.forEach = function (cb) {
