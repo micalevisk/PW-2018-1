20f31efc2a6ffdcb7c418d8f1909b2474fa8bf2d
Micael Levi <mllc@icomp.ufam.edu.br>
2018-06-28 01:11:04 -0400

    up: botão linkando a página do jogo

--- YII/advanced/frontend/views/site/index.php
+++ YII/advanced/frontend/views/site/index.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 
@@ -11,6 +12,6 @@ $this->title = YII::$app->name;
         <?= Html::img('@web/img/logo_icomp.png',['width'=>'400']) ?>
         <p class="lead">O Instituto de Computação (IComp), antigo Departamento de Ciência da Computação (DCC),  é um instituto acadêmico que agrega os professores da área de computação. Como todo instituto acadêmico o IComp atua no ensino, pesquisa e extensão, além de desempenhar atividades administrativas.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href=" <?= Url::to(['jogo/index']) ?> ">Clique aqui para Jogar o Skifree</a></p>
     </div>
 </div>
