013e050b2a3a655d39ff13825f82b2117e14db66
Micael Levi <mllc@icomp.ufam.edu.br>
2018-06-29 18:18:47 -0400

    up: indicador do usuário a ser atualizado

--- YII/advanced/frontend/views/user/update.php
+++ YII/advanced/frontend/views/user/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 
-$this->title = 'Atualizar o Usuário ' . $model->id;
+$this->title = 'Atualizar o Usuário "' . $model->username . '"';
 $this->params['breadcrumbs'][] = ['label' => 'Usuários', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->username, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Atualizar';
 ?>
 <div class="user-update">
