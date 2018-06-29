017ce41c086ed7bd5161330be5b5d4079dfb02a2
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2018-06-29 18:40:27 -0400

    up: regra 02 pronta

--- YII/advanced/frontend/views/site/index.php
+++ YII/advanced/frontend/views/site/index.php
@@ -12,6 +12,6 @@ $this->title = YII::$app->name;
         <?= Html::img('@web/img/logo_icomp.png',['width'=>'400']) ?>
         <p class="lead">O Instituto de Computação (IComp), antigo Departamento de Ciência da Computação (DCC),  é um instituto acadêmico que agrega os professores da área de computação. Como todo instituto acadêmico o IComp atua no ensino, pesquisa e extensão, além de desempenhar atividades administrativas.</p>
 
-        <p><a class="btn btn-lg btn-success" href=" <?= Url::to(['jogo/index']) ?> ">Clique aqui para Jogar o Skifree</a></p>
+        <p><a class="btn btn-lg btn-success" href=" <?= Url::to(['jogo/index']) ?> ">Iniciar Jogo</a></p>
     </div>
 </div>
