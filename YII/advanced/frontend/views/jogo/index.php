23927c91dc3ec642246b5508630ab47b3b64aaf5
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2018-07-04 18:55:23 -0400

    fix: disposição do painel e do jogo

--- YII/advanced/frontend/views/jogo/index.php
+++ YII/advanced/frontend/views/jogo/index.php
@@ -37,32 +37,44 @@ function registrarPontuacao(pontuacao) {
 
 <h1 class="text-center"><?= $this->title ?></h1>
 
-<div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-4 col-sm-push-8" style="padding:0%;">
 
-  <panel id="infoBox">
-      <p><code>
-        &copy; <a href="https://github.com/micalevisk/ProgWeb" target="_blank" rel="noopener">Micael Levi</a>
-      </code></p><br>
-      <p>Use o <kbd>space</kbd> para pausar/despausar/iniciar o jogo.</p>
-      <p>Use o <kbd>F</kbd> para aumentar/diminuir a velocidade do esquiador.</p>
-      <p>Use <kbd>&larr;</kbd> e <kbd>&rarr;</kbd>  ou <kbd>A</kbd> e <kbd>D</kbd> para controlar o esquiador.</p>
-      <br>
-      <p class="label" data-identifier="FPS"   data-sufix="" id="fps"></p>
-      <p class="label" data-identifier="Andou" data-sufix="m" id="andado"></p>
-      <p class="label" data-identifier="Vidas" data-sufix=" restantes" id="vidas"></p>
-  </panel>
+      <div class="panel panel-primary" id="infoBox">
+        <div class="panel-body">
+          <p>Use o <kbd>space</kbd> para pausar/despausar/iniciar o jogo.</p>
+          <p>Use o <kbd>F</kbd> para aumentar/diminuir a velocidade do esquiador.</p>
+          <p>Use <kbd>&larr;</kbd> e <kbd>&rarr;</kbd>  ou <kbd>A</kbd> e <kbd>D</kbd> para controlar o esquiador.</p>
 
-  <div id="tabuleiro">
-    <div id="skier"></div>
+          <br>
+          <p class="infolabel" data-identifier="FPS" id="fps"></p>
+          <p class="infolabel" data-identifier="Andou" data-sufix="m" id="andado"></p>
+          <p class="infolabel" data-identifier="Vidas Restantes" id="vidas"></p>
+        </div>
+        <div class="panel-footer">
+          <code>
+          &copy; <a href="https://github.com/micalevisk/ProgWeb" target="_blank" rel="noopener">Micael Levi</a>
+          </code>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-sm-8 col-sm-pull-4">
+
+    <div class="panel panel-primary" id="tabuleiro">
+      <div id="skier"></div>
+    </div>
+
+    </div>
   </div>
-
 </div>
 
 <?php
 
-$this->registerCssFile( $skifreepaths['css'] . 'layout.css', ['position' => $this::POS_END] );
-$this->registerCssFile( $skifreepaths['css'] . 'skier.css', ['position' => $this::POS_END] );
-$this->registerCssFile( $skifreepaths['css'] . 'obstacles.css', ['position' => $this::POS_END] );
+$this->registerCssFile( $skifreepaths['css'] . 'layout.css' );
+$this->registerCssFile( $skifreepaths['css'] . 'skier.css' );
+$this->registerCssFile( $skifreepaths['css'] . 'obstacles.css' );
 
 $this->registerJsFile( $skifreepaths['lib'] . 'utils.js');
 $this->registerJsFile( $skifreepaths['models'] . 'ObjectPool.js');
